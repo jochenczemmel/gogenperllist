c8bffde0cac15e155cb9c6c22ce0262b214dae7a
czemmel <jochen.czemmel@analytical-software.de>
2017-03-12 19:56:37 +0100

    Listengenerator
    Push, Pop, Shift, Unshift, Len
    
    evtl. wäre noch gut: func Foreach(func(para <Mytype))

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"text/template"
+)
+
+var code = `
+package {{.Package}}
+
+type {{.MyType}}List struct {
+	elements []{{.MyType}}
+}
+
+func NewIntList() *{{.MyType}}List {
+	return &{{.MyType}}List{[]{{.MyType}}{}}
+}
+
+func (liste *{{.MyType}}List) Len() {{.MyType}} {
+	return len(liste.elements)
+}
+
+func (liste *{{.MyType}}List) Push(element {{.MyType}}) {
+	liste.elements = append(liste.elements, element)
+}
+
+func (liste *{{.MyType}}List) Unshift(element {{.MyType}}) {
+	newl := make([]{{.MyType}}, 0, len(liste.elements)+1)
+	newl = append(newl, element)
+	newl = append(newl, liste.elements...)
+	liste.elements = newl
+}
+
+func (liste *{{.MyType}}List) Shift() {{.MyType}} {
+	if len(liste.elements) == 0 {
+		r := new({{.MyType}})
+		return *r
+	}
+	first := liste.elements[0]
+	liste.elements = liste.elements[1:]
+	return first
+}
+
+func (liste *{{.MyType}}List) Pop() {{.MyType}} {
+	if len(liste.elements) == 0 {
+		r := new({{.MyType}})
+		return *r
+	}
+	lastIndex := len(liste.elements) - 1
+	last := liste.elements[lastIndex]
+	liste.elements = liste.elements[:lastIndex]
+	return last
+}
+`
+
+func main() {
+	parsedTemplate := template.Must(template.New("liste").Parse(code))
+
+	for i := 1; i < len(os.Args); i++ {
+		filename := os.Getenv("GOFILE") + os.Args[i] + "_liste_generated.go"
+		fid, err := os.Create(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: could not create file %s: %s\n",
+				filename, err)
+			continue
+		}
+		parameter := map[string]string{
+			"MyType":  os.Args[i],
+			"Package": os.Getenv("GOPACKAGE"),
+		}
+		parsedTemplate.Execute(fid, parameter)
+		fid.Close()
+	}
+}
