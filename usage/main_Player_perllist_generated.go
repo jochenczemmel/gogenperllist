b6db928d3fcaaa452f261dac32faf8eabc26529a
czemmel <jochen.czemmel@analytical-software.de>
2017-10-01 19:51:24 +0200

    newly created generated file

--- usage/main_Player_perllist_generated.go
+++ usage/main_Player_perllist_generated.go
@@ -1,30 +1,39 @@
 package main
 
+// PlayerList contains a slice of Player values
 type PlayerList struct {
 	elements []Player
 }
 
+// NewPlayerList creates a new List of Player values
+// with optional initial values
 func NewPlayerList(values ...Player) *PlayerList {
 	return &PlayerList{values}
 }
 
+// Len returns the length of the underlying slice
 func (l *PlayerList) Len() int {
 	return len(l.elements)
 }
 
+// Cap returns the capacity of the underlying slice
 func (l *PlayerList) Cap() int {
 	return cap(l.elements)
 }
 
+// Push appends an element at the end of the list
 func (l *PlayerList) Push(element Player) {
 	l.elements = append(l.elements, element)
 }
 
+// Unshift adds an element at the beginning of the list
 func (l *PlayerList) Unshift(element Player) {
 	newl := append([]Player{element}, l.elements...)
 	l.elements = newl
 }
 
+// Shift returns the first element and removes it from the list
+// returns zero value if list is empty
 func (l *PlayerList) Shift() Player {
 	if len(l.elements) == 0 {
 		r := new(Player)
@@ -35,6 +44,8 @@ func (l *PlayerList) Shift() Player {
 	return first
 }
 
+// Pop returns the last element and removes it from the list
+// returns zero value if list is empty
 func (l *PlayerList) Pop() Player {
 	if len(l.elements) == 0 {
 		r := new(Player)
@@ -46,12 +57,17 @@ func (l *PlayerList) Pop() Player {
 	return last
 }
 
+// Foreach iterates over the slice by calling the given function
+// providing the slice index and the value
 func (l *PlayerList) Foreach(f func(int, Player)) {
 	for index, value := range l.elements {
 		f(index, value)
 	}
 }
 
+// Pop returns the last element and removes it from the list
+// returns the value and true if the list contains values,
+// else the zero value and false
 func (l *PlayerList) PopChecked() (Player, bool) {
 	if len(l.elements) == 0 {
 		r := new(Player)
@@ -60,6 +76,9 @@ func (l *PlayerList) PopChecked() (Player, bool) {
 	return l.Pop(), true
 }
 
+// Shift returns the first element and removes it from the list
+// returns the value and true if the list contains values,
+// else the zero value and false
 func (l *PlayerList) ShiftChecked() (Player, bool) {
 	if len(l.elements) == 0 {
 		r := new(Player)
@@ -68,6 +87,8 @@ func (l *PlayerList) ShiftChecked() (Player, bool) {
 	return l.Shift(), true
 }
 
+// ElementAt returns the element at the given position.
+// It panics if index is invalid (default slice behaviour)
 func (l *PlayerList) ElementAt(index int) Player {
 	return l.elements[index]
 }
