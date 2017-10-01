9c26e04f55b3e02b676a5dd8cb2ebfdba7f84a44
czemmel <jochen.czemmel@analytical-software.de>
2017-10-01 09:38:38 +0200

    Template angepaßt
    an die Änderungen in perllist

--- main.go
+++ main.go
@@ -13,58 +13,71 @@ type {{.MyType}}List struct {
 	elements []{{.MyType}}
 }
 
-func New{{.MyType}}List() *{{.MyType}}List {
-	return &{{.MyType}}List{[]{{.MyType}}{}}
+// NewIntList creates a new list with optional initial values
+func NewIntList(values ...{{.MyType}}) *{{.MyType}}List {
+	return &{{.MyType}}List{values}
 }
 
-func (liste *{{.MyType}}List) Len() int {
-	return len(liste.elements)
+// return len of list
+func (l *{{.MyType}}List) Len() int {
+	return len(l.elements)
 }
 
-func (liste *{{.MyType}}List) Push(element {{.MyType}}) {
-	liste.elements = append(liste.elements, element)
+// return cap of list
+func (l *{{.MyType}}List) Cap() int {
+	return cap(l.elements)
 }
 
-func (liste *{{.MyType}}List) Unshift(element {{.MyType}}) {
-	newl := make([]{{.MyType}}, 0, len(liste.elements)+1)
-	newl = append(newl, element)
-	newl = append(newl, liste.elements...)
-	liste.elements = newl
+// append element to list
+func (l *{{.MyType}}List) Push(element {{.MyType}}) {
+	l.elements = append(l.elements, element)
 }
 
-func (liste *{{.MyType}}List) Shift() {{.MyType}} {
-	if len(liste.elements) == 0 {
+// prepend element to list
+func (l *{{.MyType}}List) Unshift(element {{.MyType}}) {
+	newl := append([]{{.MyType}}{element}, l.elements...)
+	l.elements = newl
+}
+
+// get first element of list
+// or zero value if list is empty
+func (l *{{.MyType}}List) Shift() {{.MyType}} {
+	if len(l.elements) == 0 {
 		r := new({{.MyType}})
 		return *r
 	}
-	first := liste.elements[0]
-	liste.elements = liste.elements[1:]
+	first := l.elements[0]
+	l.elements = l.elements[1:]
 	return first
 }
 
-func (liste *{{.MyType}}List) Pop() {{.MyType}} {
-	if len(liste.elements) == 0 {
+// get last element of list
+// or zero value if list is empty
+func (l *{{.MyType}}List) Pop() {{.MyType}} {
+	if len(l.elements) == 0 {
 		r := new({{.MyType}})
 		return *r
 	}
-	lastIndex := len(liste.elements) - 1
-	last := liste.elements[lastIndex]
-	liste.elements = liste.elements[:lastIndex]
+	lastIndex := len(l.elements) - 1
+	last := l.elements[lastIndex]
+	l.elements = l.elements[:lastIndex]
 	return last
 }
 
-func (liste *{{.MyType}}List) Foreach(f func(int, {{.MyType}})) {
-	for index, value := range liste.elements {
+// iterate over list elements
+func (l *{{.MyType}}List) Foreach(f func({{.MyType}}, {{.MyType}})) {
+	for index, value := range l.elements {
 		f(index, value)
 	}
-}`
+}
+`
 
 func main() {
-	parsedTemplate := template.Must(template.New("liste").Parse(code))
+	parsedTemplate := template.Must(template.New("perllist").Parse(code))
 
 	for i := 1; i < len(os.Args); i++ {
 		filename := strings.TrimSuffix(os.Getenv("GOFILE"), ".go") + "_" +
-			strings.ToLower(os.Args[i]) + "_liste_generated.go"
+			os.Args[i] + "_perllist_generated.go"
 		fid, err := os.Create(filename)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "ERROR: could not create file %s: %s\n",
